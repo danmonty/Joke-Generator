5a6caecb7dda562bd82aa0afe7a11d8b814eef1d
danmonty <danmont00@gmail.com>
2018-04-12 16:14:11 -0600

    create joke generator

---
+++ array.js
@@ -0,0 +1,20 @@
+// how to make array
+
+var array =[]
+
+var array2 = new Array()
+
+var newArray = new Array(100)
+
+newArray.length
+
+var array = ['This', 'is', 'an', 'array']
+
+var ppl = []
+ppl.push ('tom hanks')
+//use to add things to arrays
+ppl.unshift ('jap pan')
+//add to the begining
+ppl.pop
+ppl.shift
+delete ppl[2]
